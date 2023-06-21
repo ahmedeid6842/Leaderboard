580cd02b97d3d84ab8c96da34f7b9d574f996857
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-21 23:33:09 +0300

    Sorted the scores before display them

--- src/modules/displayScores.js
+++ src/modules/displayScores.js
@@ -1,6 +1,11 @@
 const display = (scores) => {
   const scoreListContainer = document.querySelector(".score-list-container");
   let scoresListHtml = ``;
+
+  //sort scores based on the score before displaying
+  scores.sort((a, b) => b.score - a.score);
+
+  // iterate over scores and added them to DOM
   scores.forEach((score) => {
     scoresListHtml += `
     <li class="score-item">${score.user}: ${score.score}</li>
