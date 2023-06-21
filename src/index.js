01ffea8e602ddde133053c880ccacac86601aaf3
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-21 23:38:22 +0300

    Added refresh button functionality

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import { getScores } from "./modules/fetching";
 import createScoreHandler from "./modules/createScore";
 
 const addScoreForm = document.querySelector("#form");
+const refreshButton = document.querySelector("#refresh-btn");
 
 getScores();
 
 addScoreForm.addEventListener("submit", createScoreHandler);
-
+refreshButton.addEventListener("click", getScores);
