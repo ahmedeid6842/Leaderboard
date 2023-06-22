b42a4ba63b1e3493157eef7ae65441d7e1e36b96
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-23 01:54:06 +0300

    Added responsive web design with status messages

--- src/modules/fetching.js
+++ src/modules/fetching.js
@@ -1,24 +1,32 @@
-import display from './displayScores.js';
+import display from "./displayScores.js";
 
 const postScore = async (body) => {
-  await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores',
-    {
-      method: 'POST',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    },
-  );
+  try {
+    await fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores",
+      {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+  } catch (error) {
+    document.querySelector(".error").classList.remove("hidden");
+  }
 };
 
 const getScores = async () => {
-  const response = await fetch(
-    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores',
-  );
-  const scores = await response.json();
-  display(scores.result);
+  try {
+    const response = await fetch(
+      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores"
+    );
+    const scores = await response.json();
+    display(scores.result);
+  } catch (error) {
+    document.querySelector(".error").classList.remove("hidden");
+  }
 };
 
 export { getScores, postScore };
