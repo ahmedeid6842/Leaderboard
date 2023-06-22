5c95c8cb4ce32873c9867407e30ced450987d6fc
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-06-23 01:58:52 +0300

    Fixed linters errors

--- src/modules/createScore.js
+++ src/modules/createScore.js
@@ -1,15 +1,15 @@
-import { postScore } from "./fetching.js";
+import { postScore } from './fetching.js';
 
 const createScoreHandler = (event) => {
   event.preventDefault();
-  const user = document.querySelector("#user");
-  const score = document.querySelector("#score");
+  const user = document.querySelector('#user');
+  const score = document.querySelector('#score');
 
   postScore({ user: user.value, score: score.value });
 
-  document.querySelector(".success").classList.remove("hidden");
-  user.value = "";
-  score.value = "";
+  document.querySelector('.success').classList.remove('hidden');
+  user.value = '';
+  score.value = '';
 };
 
 export default createScoreHandler;
--- src/modules/fetching.js
+++ src/modules/fetching.js
@@ -1,31 +1,31 @@
-import display from "./displayScores.js";
+import display from './displayScores.js';
 
 const postScore = async (body) => {
   try {
     await fetch(
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores",
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores',
       {
-        method: "POST",
+        method: 'POST',
         body: JSON.stringify(body),
         headers: {
-          "Content-type": "application/json; charset=UTF-8",
+          'Content-type': 'application/json; charset=UTF-8',
         },
-      }
+      },
     );
   } catch (error) {
-    document.querySelector(".error").classList.remove("hidden");
+    document.querySelector('.error').classList.remove('hidden');
   }
 };
 
 const getScores = async () => {
   try {
     const response = await fetch(
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores"
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/I8nlr5WBZ1UYAevVbaLR/scores',
     );
     const scores = await response.json();
     display(scores.result);
   } catch (error) {
-    document.querySelector(".error").classList.remove("hidden");
+    document.querySelector('.error').classList.remove('hidden');
   }
 };
 
